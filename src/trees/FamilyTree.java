55b75039463e158cc663c8da434fae499ec7595c
Alex Chakarov <alex.gendreau24@gmail.com>
2022-04-29 00:44:48 -0700

    error in buffered reader

--- src/trees/FamilyTree.java
+++ src/trees/FamilyTree.java
@@ -121,7 +121,7 @@ public class FamilyTree
 		// Parse the input file. Create a FileReader that reads treeFile. Create a BufferedReader
 		// that reads from the FileReader.
 		FileReader fr = new FileReader(treeFile);
-		BufferedReader br = new BufferedReader(br);
+		BufferedReader br = new BufferedReader(fr);
 		String line;
 		while ((line = br.readLine()) != null)
 			addLine(line);
